3a3e6f0b7277776a9eae27a4a7907bbf58e89035
shaynafc <shayna.cher@gmail.com>
2021-02-11 14:53:22 -0500

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,1 @@
-// API key
-const API_KEY = "pk.eyJ1IjoiZmlyZXByb29mc29ja3MiLCJhIjoiY2psd2FpbzdpMTRhczNwcGUxb2phcnRuMCJ9.l4EjUexxcYe9PEifJy2NrA";
+const API_KEY = "pk.eyJ1Ijoia2hiYWRlcjEwMSIsImEiOiJja2p6dzc0Z2EwOTd4MnVuajB4a2VlZzkyIn0.79ImyPjTxEelnflGWogJIQ";
